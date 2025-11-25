e694de2165798e3038f43da576491b023d665754
inhatc_KJS <rkdwltmd57@gmail.com>
2025-11-25 10:10:00 +0900

    fix: 오타수정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -66,7 +66,7 @@ const scrollToProjects = () => { document.getElementById('projects').scrollIntoV
 <template>
   <main>
     <section id="about" class="main-profile">
-      <img src="profile.png" alt="강지승 프로필 사진" class="profile-img">
+      <img src="/profile.png" alt="강지승 프로필 사진" class="profile-img">
       <h1>강지승</h1>
       <p>간단한 한 줄 소개</p>
       <p>전화번호: 010-7164-6663</p>
@@ -125,28 +125,28 @@ const scrollToProjects = () => { document.getElementById('projects').scrollIntoV
       <h1 class="section-title">My Skills</h1>
       <div class="skills">
         <div class="skill-item">
-          <img src="java-logo.png" alt="Java logo">
+          <img src="/java-logo.png" alt="Java logo">
           <h3>Java</h3>
           <p>숙련도: (숙련도 내용)</p>
           <p>사용 경험: (사용 경험 나열)</p>
         </div>
 
         <div class="skill-item">
-          <img src="python-logo.png" alt="Python logo">
+          <img src="/python-logo.png" alt="Python logo">
           <h3>Python</h3>
           <p>숙련도: (숙련도 내용)</p>
           <p>사용 경험: (사용 경험 나열)</p>
         </div>
 
         <div class="skill-item">
-          <img src="db-logo.png" alt="Database-logo">
+          <img src="/db-logo.png" alt="Database-logo">
           <h3>Database</h3>
           <p>숙련도: (숙련도 내용)</p>
           <p>사용 경험: (사용 경험 나열)</p>
         </div>
 
         <div class="skill-item">
-          <img src="android-logo.png" alt="Android-logo">
+          <img src="/android-logo.png" alt="Android-logo">
           <h3>Android</h3>
           <p>숙련도: (숙련도 내용)</p>
           <p>사용 경험: (사용 경험 나열)</p>
